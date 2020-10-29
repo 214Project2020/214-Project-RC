8ff8d4484e0bd8717cd3c2271b2f7d7849c17167
Kristi <70036480+Marshuu@users.noreply.github.com>
2020-10-29 08:03:20 +0200

    Update and rename track.cpp to Track.cpp

---
+++ Track.cpp
@@ -0,0 +1,45 @@
+#include "Track.h"
+#include <string>
+
+using namespace std;
+
+Track::Track(){
+	this->trackName = "";
+	this->numLaps = 0;
+	this->isEuro = false;    //this->isEuro = NULL is the same as setting it to false;
+	this->country = "";
+}
+
+Track::Track(string name, int num, bool flag, string c){
+	this->trackName = name;
+	this->numLaps = num;
+	this->isEuro = flag;
+    this->country = c;
+}
+
+string Track::getTrackName(){
+	return this->trackName;
+}
+void Track::setTrackName(string name){
+	this->trackName = name;
+}
+int Track::getNumLaps(){
+	return this->numLaps;
+}
+void Track::setNumLaps(int num){
+	this->numLaps = num;
+}
+bool Track::getIsEuro(){
+	return this->isEuro;
+}
+void Track::setIsEuro(bool flag){
+	this->isEuro = flag;
+}
+
+string Track::getCountry(){
+    return this->country;
+}
+
+void Track::setCountry(string c){
+    this->country = c;
+}
