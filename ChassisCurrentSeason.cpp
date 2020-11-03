1c6785e47dd03d8c1bd052487a5a9606a22f6303
Zachalak1999 <70139729+Zachalak1999@users.noreply.github.com>
2020-11-03 20:35:43 +0200

    Update ChassisCurrentSeason.cpp

--- ChassisCurrentSeason.cpp
+++ ChassisCurrentSeason.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 ChassisCurrentSeason::ChassisCurrentSeason(){
-    printf("Chassis Engineering department created for current season.\n");
+    cout<<"Chassis Engineering department created for current season.\n";
     
 }
 
 void ChassisCurrentSeason::ChassisImprove(){
-    printf("Using data from testing the chassis of the car of the current season has been improved.\n");
+    cout<<"Using data from testing the chassis of the car of the current season has been improved.\n";
 
 }
