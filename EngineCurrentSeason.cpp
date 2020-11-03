e17c7da35e8f6830d23c4dde060ef630586d406a
Zachalak1999 <70139729+Zachalak1999@users.noreply.github.com>
2020-11-03 20:38:24 +0200

    Update EngineCurrentSeason.cpp

--- EngineCurrentSeason.cpp
+++ EngineCurrentSeason.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 EngineCurrentSeason::EngineCurrentSeason(){
-    printf("Engine Engineering department created for current season.\n");
+    cout<<"Engine Engineering department created for current season.\n";
     
 }
 
 void EngineCurrentSeason::EngineImprove(){
-    printf("Using data from testing the engine of the car of the current season has been improved.\n");
+    cout<<"Using data from testing the engine of the car of the current season has been improved.\n";
 
 }
