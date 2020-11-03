f811ede02de42d8774656d34efe883dc71cab02f
Zachalak1999 <70139729+Zachalak1999@users.noreply.github.com>
2020-11-03 20:36:28 +0200

    Update ChassisNextSeason.cpp

--- ChassisNextSeason.cpp
+++ ChassisNextSeason.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 ChassisNextSeason::ChassisNextSeason(){
-    printf("Chassis Engineering department created for next season.\n");
+    cout<<"Chassis Engineering department created for next season.\n";
     
 }
 
 void ChassisNextSeason::ChassisImprove(){
-    printf("Using data from testing the chassis of the car of the next season has been improved.\n");
+    cout<<"Using data from testing the chassis of the car of the next season has been improved.\n";
 
 }
