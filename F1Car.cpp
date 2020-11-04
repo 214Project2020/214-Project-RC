33fa096b0ad5e07993994c97ce72341d4b9c224f
Zachalak1999 <70139729+Zachalak1999@users.noreply.github.com>
2020-11-04 14:22:40 +0200

    Update F1Car.cpp

--- F1Car.cpp
+++ F1Car.cpp
@@ -6,39 +6,6 @@ F1Car::F1Car(){
 
 }
 
-/*F1Car::F1Car(CurrentSeasonCar *carCS){
-    chas = carCS->getChas();
-    engi = carCS->getEngi();
-    aero = carCS->getAero();
-    elec = carCS->getElec();
-}
-
-F1Car::F1Car(NextSeasonCar *carNS){
-    chas = carNS->getChas();
-    engi = carNS->getEngi();
-    aero = carNS->getAero();
-    elec = carNS->getElec();
-}
-*/
-
 F1Car::~F1Car(){
     
 }
-
-/*
-void F1Car::aeroImprover(){
-    aero->AeroImprove();
-}
-
-void F1Car::elecImprover(){
-    elec->ElecImprove();
-}
-
-void F1Car::chasImprover(){
-    chas->ChassisImprove();
-}
-
-void F1Car::engiImprover(){
-    engi->EngineImprove();
-}
-*/
