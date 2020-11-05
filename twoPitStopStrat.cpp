97db255f444e201a3a162a7ee59430ad1714ed15
vashistlalla <72375090+vashistlalla@users.noreply.github.com>
2020-11-05 18:08:54 +0200

    Update twoPitStopStrat.cpp

--- twoPitStopStrat.cpp
+++ twoPitStopStrat.cpp
@@ -1,6 +1,7 @@
 #include "racingStrategyState.h"
 
 void twoPitStopStrat::handleState(){
+	cout << "Two pit stop strategy:" << endl;
 	this->getSession()->setPractice(0, 1, 1);
 	this->getSession()->setQuali(1,0,0);
 	this->getSession()->setRace(1,1,0);
