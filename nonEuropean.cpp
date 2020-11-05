e7391298bdd0f415d334a99474401d7173cb1698
vashistlalla <72375090+vashistlalla@users.noreply.github.com>
2020-11-05 18:00:39 +0200

    Update nonEuropean.cpp

--- nonEuropean.cpp
+++ nonEuropean.cpp
@@ -10,7 +10,7 @@ void nonEuropean::preperation(){
 	   cout << "------------------------------------------" << endl;
     cout << "STARTING RACE IN " << this->getTrack()->getCountry() << endl;// getTrack() is from the parent class Logistics
     cout << "Category: Non-European" << endl;
-    cout << "The car is transported by charted plane to track";
+    cout << "The car is transported by charted plane to "<< this->getTrack()->getTrackName() << endl;
     transportContainers();
     cout << "------------------------------------------" << endl;
 }
